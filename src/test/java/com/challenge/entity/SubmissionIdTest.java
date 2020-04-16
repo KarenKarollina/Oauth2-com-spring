d72ddb52c6b1e189fa7f2638b42118529d444d28
KarenKarollina <59506944+KarenKarollina@users.noreply.github.com>
2020-04-16 11:25:21 -0300

    Add files via upload

---
+++ src/test/java/com/challenge/entity/SubmissionIdTest.java
@@ -0,0 +1,14 @@
+package com.challenge.entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SubmissionIdTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
